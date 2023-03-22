c39fc4fadee241407bb5b4512a0eab0a565a5e1a
daniel <danielvdriet@gmail.com>
2023-03-22 11:33:43 +0100

    Added 'daily_std' to models

--- inflammation/models.py
+++ inflammation/models.py
@@ -45,3 +45,12 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+def daily_std(data):
+    """Calculate the daily standard deviation of a 2D inflammation data array.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all day
+    :returns: An array of standard deviation values of measurements for each day.
+    """
+
+    return np.std(data, axis=0)
+
